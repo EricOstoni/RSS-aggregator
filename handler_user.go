8a34e8558f2d0fec0ee4b6399621c21301b93c03
EricOstoni <eostoni@student.unipu.hr>
2024-05-09 03:05:39 +0200

    add handler get user

--- handler_user.go
+++ handler_user.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/EricOstoni/RSS-aggregator/internal/auth"
 	"github.com/EricOstoni/RSS-aggregator/internal/database"
 	"github.com/google/uuid"
 )
@@ -35,5 +36,19 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
+		return
+	}
 	respondWithJSON(w, 200, databaseUserToUser(user))
 }
