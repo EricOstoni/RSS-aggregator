48d266450c84cce8f9109d71f9c0678040b502e6
EricOstoni <eostoni@student.unipu.hr>
2024-05-21 19:28:36 +0200

    add user can follow the feed

---
+++ handler_feeds_follows.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/EricOstoni/RSS-aggregator/internal/database"
+	"github.com/google/uuid"
+)
+
+func (apiCfg *apiConfig) handlerCreateFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+
+	type parameters struct {
+		FeedID uuid.UUID `json:"feed_id"`
+	}
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error parsing JSON: %v", err))
+		return
+	}
+
+	feedFollow, err := apiCfg.DB.CreateFeedFollow(r.Context(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().Local(),
+		UpdatedAt: time.Now().Local(),
+		FeedID:    params.FeedID,
+		UserID:    user.ID,
+	})
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't create feed: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseFeedFollowToFeedFollow(feedFollow))
+}
